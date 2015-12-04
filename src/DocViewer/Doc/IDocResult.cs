43fd2b5442905e2120297311fb022a6c7674c8f4
莴笋Ho <gzhehai@foxmail.com>
2015-12-04 18:51:06 +0800

    add a file IDocResult.cs

---
+++ src/DocViewer/Doc/IDocResult.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocViewer.Doc
+{
+    using DocViewer.Impl;
+    public interface IDocResult : IResult
+    {
+    }
+}
